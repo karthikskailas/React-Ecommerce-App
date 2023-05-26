e58ddba7520d951a7e8aadf712ef72858d52e4df
Karthik S Kailas <karthikskailas@gmail.com>
2023-05-26 11:23:53 +0530

    remove all the imports and jsx  inside the return function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div className="App">
+			<p>hello</p>
+		</div>
+	);
+};
 
 export default App;
