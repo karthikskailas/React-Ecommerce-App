aac7e4cd3643e335bc674174c4fcc4414865aa02
karthik s kailas <karthikskailas@gmail.com>
2023-05-27 05:48:49 -0700

    fixed whitescreen issue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./router/homepage/homepage.component";
 const App = () => {
-	<Routes>
-		<Route path="/" element={<HomePage />} />
-	</Routes>;
+	return (
+		<Routes>
+			<Route path="/" element={<HomePage />} />
+		</Routes>
+	);
 };
 
 export default App;
