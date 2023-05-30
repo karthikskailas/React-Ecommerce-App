d0938f3634b881d5a9e4a3470802714989c4a0ef
karthik s kailas <karthikskailas@gmail.com>
2023-05-30 10:50:26 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./router/homepage/homepage.component";
+import Navigation from "./router/navigation/navigation.component";
+import Shop from "./router/shop/shop.component";
+
+
 const App = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<HomePage />} />
+			<Route path="/" element={<Navigation />}>
+				<Route index element={<HomePage />} />
+				<Route path="/shop" element={<Shop/>} />
+			</Route>
 		</Routes>
 	);
 };
