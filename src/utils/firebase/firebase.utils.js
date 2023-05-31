2d3a878ed40eb272a5a243a4bb651a065007ef03
karthik s kailas <karthikskailas@gmail.com>
2023-05-31 10:34:57 +0530

    add firebase libary and set the google authentication and add functionality to create a new document in firebase database

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,52 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+const firebaseConfig = {
+	apiKey: "AIzaSyBeWyrQ61T0YTu_r8H_YMr5Reqw7kDBonc",
+	authDomain: "ecommerce-tutorial-db.firebaseapp.com",
+	projectId: "ecommerce-tutorial-db",
+	storageBucket: "ecommerce-tutorial-db.appspot.com",
+	messagingSenderId: "556946000668",
+	appId: "1:556946000668:web:75529fc79313405b4c48d9",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters = {
+	prompt: "select_account",
+};
+
+export const auth = getAuth();
+export const signInWithGooglePopups = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore(); // database instance
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	const userDocRef = await doc(db, "users", userAuth.uid); // instance  of a document model doc(database,collection,Identifier);
+	const userSnapshot = await getDoc(userDocRef);
+
+	if (!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt,
+			});
+		} catch (err) {
+			console.log(`There is an error: ${err.message}`);
+		}
+	}
+
+	return userDocRef;
+
+	//if user doesn't exist
+	//if user does exist
+
+	//userSnapshot allow as to access data
+	//userSnapshot.exists() method allow us to check whether or not there is and instance of it that exists inside of our database
+};
