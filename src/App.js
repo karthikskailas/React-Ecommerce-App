e8482f2f0f3de5dc1291899a9e43351cb6a5a86d
karthik s kailas <karthikskailas@gmail.com>
2023-05-31 10:35:13 +0530

    add sign in route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./router/homepage/homepage.component";
 import Navigation from "./router/navigation/navigation.component";
 import Shop from "./router/shop/shop.component";
+import SignIn from "./router/sign-in/sign-in.component";
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
 			<Route path="/" element={<Navigation />}>
 				<Route index element={<HomePage />} />
 				<Route path="/shop" element={<Shop/>} />
+				<Route path="/Sign-In" element={<SignIn/>} />
 			</Route>
 		</Routes>
 	);
